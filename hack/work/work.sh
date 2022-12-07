a426d8e76c0d99241051170e1e608a963fc3626e
Zoran Regvart <zoran@regvart.com>
2022-12-07 15:01:26 +0100

    Allow customizing the `work` namespace name
    Given environment variable `WORK_NAMESPACE` the `"work"` namespace can
    be named differently. Follows the same principle of implementing this
    via KEP function for Kustomize and replacing the values in a shell
    script and using the `replacements` Kustomization.

---
+++ hack/work/work.sh
@@ -0,0 +1,29 @@
+#!/bin/env bash
+# Copyright 2022 Red Hat, Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+
+#
+# SPDX-License-Identifier: Apache-2.0
+
+# Fetches the Tekton YAML descriptors for the version we depend on
+
+set -o errexit
+set -o pipefail
+set -o nounset
+
+NS="${WORK_NAMESPACE:-work}"
+
+printf -- '---
+apiVersion: v1
+kind: Namespace
+metadata:
+  name: %s
+---
+apiVersion: v1
+kind: ConfigMap
+metadata:
+  name: work-namespace-name
+  namespace: work
+data:
+  NAMESPACE: %s' "${NS}" "${NS}"
