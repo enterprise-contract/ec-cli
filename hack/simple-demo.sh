16fe44f298e7d1e59b28164135d258ae1895414c
cuipinghuo <chuo@redhat.com>
2023-01-09 15:39:04 -0500

    update PIPELINE_BUNDLE in rebuild.sh and ECP to use new source schema

--- hack/simple-demo.sh
+++ hack/simple-demo.sh
@@ -15,12 +15,15 @@
 #
 # SPDX-License-Identifier: Apache-2.0
 
-IMAGE=${IMAGE:-"quay.io/cuipinghuo/single-container-app:9f5d549dd64aacf10e3baac90972dfd5df788324"}
+IMAGE=${IMAGE:-"quay.io/redhat-appstudio/ec-golden-image:latest"}
 
+#ec-golden-image is signed with staging public key, to verify, use the below public key
+#(https://raw.githubusercontent.com/redhat-appstudio/infra-deployments/main/components/pipeline-service/public/tekton-chains-signing-secret.pub)
 PUBLIC_KEY=${PUBLIC_KEY:-"-----BEGIN PUBLIC KEY-----
-MFkwEwYHKoZIzj0CAQYIKoZIzj0DAQcDQgAEPfwkY/ru2JRd6FSqIp7lT3gzjaEC
-EAg+paWtlme2KNcostCsmIbwz+bc2aFV+AxCOpRjRpp3vYrbS5KhkmgC1Q==
+MFkwEwYHKoZIzj0CAQYIKoZIzj0DAQcDQgAE+mypw1Z/vERWHHFhdNuhB/FQd1Iu
+LKDQEdGmyiilvgAeMB6XyLcyssIxlfonnXTgU2BP0DV9sSnbRId6+9oAiw==
 -----END PUBLIC KEY-----"}
+
 
 POLICY='{
   "publicKey": "'${PUBLIC_KEY//$'\n'/\\n}'",
