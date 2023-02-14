0788c6f1596941a1f3db63d65be43a4dd77d4a3b
Luiz Carvalho <lucarval@redhat.com>
2023-02-14 14:18:37 -0500

    Update local clone of infra-deployments
    https://issues.redhat.com/browse/HACBS-1773
    
    Signed-off-by: Luiz Carvalho <lucarval@redhat.com>

---
+++ hack/update-infra-deployments.sh
@@ -0,0 +1,43 @@
+#!/usr/bin/env bash
+# Copyright 2022 Red Hat, Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+# SPDX-License-Identifier: Apache-2.0
+
+# Updates a local clone of redhat-appstudio/infra-deployments to use the latest
+# packages produced by this repository.
+# Usage:
+#   update-infra-deployments.sh <PATH_TO_INFRA_DEPLOYMENTS> [<TAG>]
+
+set -o errexit
+set -o pipefail
+set -o nounset
+
+TARGET_DIR="${1}"
+cd "${TARGET_DIR}" || exit 1
+
+echo 'Resolving task bundle...'
+TASK_BUNDLE_TAG="${2:-snapshot}"
+TASK_BUNDLE_DIGEST="$(skopeo inspect "docker://quay.io/hacbs-contract/ec-task-bundle:${TASK_BUNDLE_TAG}" | jq -r .Digest)"
+TASK_BUNDLE_REF="quay.io/hacbs-contract/ec-task-bundle:${TASK_BUNDLE_TAG}@${TASK_BUNDLE_DIGEST}"
+echo "Resolved bundle is ${TASK_BUNDLE_REF}"
+
+echo 'Updating infra-deployments...'
+REF="${TASK_BUNDLE_REF}" yq e -i \
+    '.configMapGenerator[] |=
+        select(.name == "ec-defaults").literals[] |=
+            select(. == "verify_ec_task_bundle=*") = "verify_ec_task_bundle=" + env(REF)' \
+    components/enterprise-contract/kustomization.yaml
+
+echo 'infra-deployments updated successfully'
