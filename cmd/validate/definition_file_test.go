9eff331fc4fa61f4cd102f445a44306fcd117f81
Joe Stuart <jstuart@redhat.com>
2023-03-02 15:59:53 -0600

    tests for definition-file cmd
    more tests

---
+++ cmd/validate/definition_file_test.go
@@ -0,0 +1,201 @@
+// Copyright 2022 Red Hat, Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+//
+// SPDX-License-Identifier: Apache-2.0
+
+//go:build unit
+
+package validate
+
+import (
+	"bytes"
+	"context"
+	"errors"
+	"testing"
+
+	hd "github.com/MakeNowJust/heredoc"
+	"github.com/open-policy-agent/conftest/output"
+	"github.com/spf13/afero"
+	"github.com/stretchr/testify/assert"
+
+	output2 "github.com/hacbs-contract/ec-cli/internal/output"
+	"github.com/hacbs-contract/ec-cli/internal/policy/source"
+	"github.com/hacbs-contract/ec-cli/internal/utils"
+)
+
+func TestValidateDefinitionFileCommandOutput(t *testing.T) {
+	validate := func(_ context.Context, fpath string, _ []source.PolicySource) (*output2.Output, error) {
+		return &output2.Output{PolicyCheck: []output.CheckResult{{FileName: fpath}}}, nil
+	}
+
+	cmd := validateDefinitionFileCmd(validate)
+
+	var out bytes.Buffer
+	cmd.SetOut(&out)
+
+	cmd.SetArgs([]string{
+		"--pipeline-file",
+		"/path/file1.yaml",
+		"--pipeline-file",
+		"/path/file2.yaml",
+	})
+
+	err := cmd.Execute()
+	assert.NoError(t, err)
+
+	assert.JSONEq(t, `[
+		{
+		  "filename": "/path/file1.yaml",
+		  "success": true,
+		  "violations": [],
+		  "warnings": []
+		},
+		{
+		  "filename": "/path/file2.yaml",
+		  "success": true,
+		  "violations": [],
+		  "warnings": []
+		}
+	  ]`, out.String())
+}
+
+func TestValidateDefinitionFilePolicySources(t *testing.T) {
+	expected := []source.PolicySource{
+		&source.PolicyUrl{Url: "spam-policy-source", Kind: source.PolicyKind},
+		&source.PolicyUrl{Url: "ham-policy-source", Kind: source.PolicyKind},
+		&source.PolicyUrl{Url: "bacon-data-source", Kind: source.DataKind},
+		&source.PolicyUrl{Url: "eggs-data-source", Kind: source.DataKind},
+	}
+	validate := func(_ context.Context, fpath string, sources []source.PolicySource) (*output2.Output, error) {
+		assert.Equal(t, expected, sources)
+		return &output2.Output{}, nil
+	}
+
+	cmd := validateDefinitionFileCmd(validate)
+
+	var out bytes.Buffer
+	cmd.SetOut(&out)
+
+	cmd.SetArgs([]string{
+		"--pipeline-file",
+		"/path/file1.yaml",
+		"--policy",
+		"spam-policy-source",
+		"--policy",
+		"ham-policy-source",
+		"--data",
+		"bacon-data-source",
+		"--data",
+		"eggs-data-source",
+	})
+
+	err := cmd.Execute()
+	assert.NoError(t, err)
+}
+
+func TestDefinitionFileOutputFormats(t *testing.T) {
+	testJSONText := `[{"filename":"/path/file1.yaml","violations":[],"warnings":[],"success":true}]`
+
+	testYAMLTest := hd.Doc(`
+	- filename: /path/file1.yaml
+	  success: true
+	  violations: []
+	  warnings: []
+	`)
+
+	cases := []struct {
+		name           string
+		output         []string
+		expectedFiles  map[string]string
+		expectedStdout string
+	}{
+		{
+			name:           "default output",
+			expectedStdout: testJSONText,
+		},
+		{
+			name:           "json stdout",
+			output:         []string{"--output", "json"},
+			expectedStdout: testJSONText,
+		},
+		{
+			name:           "yaml stdout",
+			output:         []string{"--output", "yaml"},
+			expectedStdout: testYAMLTest,
+		},
+		{
+			name:           "json and yaml to file",
+			output:         []string{"--output", "json=out.json", "--output", "yaml=out.yaml"},
+			expectedStdout: "",
+			expectedFiles: map[string]string{
+				"out.json": testJSONText,
+				"out.yaml": testYAMLTest,
+			},
+		},
+	}
+
+	for _, c := range cases {
+		t.Run(c.name, func(t *testing.T) {
+			fs := afero.NewMemMapFs()
+			validate := func(_ context.Context, fpath string, sources []source.PolicySource) (*output2.Output, error) {
+				return &output2.Output{PolicyCheck: []output.CheckResult{{FileName: fpath}}}, nil
+			}
+
+			cmd := validateDefinitionFileCmd(validate)
+
+			var out bytes.Buffer
+			cmd.SetOut(&out)
+
+			cmd.SetArgs(append([]string{
+				"--pipeline-file",
+				"/path/file1.yaml",
+			}, c.output...))
+
+			cmd.SetContext(utils.WithFS(context.Background(), fs))
+
+			err := cmd.Execute()
+			assert.NoError(t, err)
+			assert.Equal(t, c.expectedStdout, out.String())
+
+			for name, expectedText := range c.expectedFiles {
+				actualText, err := afero.ReadFile(fs, name)
+				assert.NoError(t, err)
+				assert.Equal(t, expectedText, string(actualText))
+			}
+		})
+	}
+}
+
+func TestValidateDefinitionFileCommandErrors(t *testing.T) {
+	validate := func(_ context.Context, fpath string, _ []source.PolicySource) (*output2.Output, error) {
+		return nil, errors.New(fpath)
+	}
+
+	cmd := validateDefinitionFileCmd(validate)
+
+	var out bytes.Buffer
+	cmd.SetOut(&out)
+	cmd.SilenceUsage = true
+
+	cmd.SetArgs([]string{
+		"--pipeline-file",
+		"/path/file1.yaml",
+		"--pipeline-file",
+		"/path/file2.yaml",
+	})
+
+	err := cmd.Execute()
+	assert.Error(t, err, "2 errors occurred:\n\t* /path/file1.yaml\n\t* /path/file2.yaml\n")
+	assert.Equal(t, "", out.String())
+}
---
+++ internal/definitionfile/validate_test.go
@@ -0,0 +1,109 @@
+// Copyright 2022 Red Hat, Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.`
+// See the License for the specific language governing permissions and
+// limitations under the License.
+//
+// SPDX-License-Identifier: Apache-2.0
+
+package definitionfile
+
+import (
+	"context"
+	"errors"
+	"fmt"
+	"testing"
+
+	conftestout "github.com/open-policy-agent/conftest/output"
+	"github.com/spf13/afero"
+	"github.com/stretchr/testify/assert"
+
+	"github.com/hacbs-contract/ec-cli/internal/evaluation_target/definitionfile"
+	"github.com/hacbs-contract/ec-cli/internal/output"
+	"github.com/hacbs-contract/ec-cli/internal/policy/source"
+	"github.com/hacbs-contract/ec-cli/internal/utils"
+)
+
+type mockEvaluator struct{}
+type badMockEvaluator struct{}
+
+func (e mockEvaluator) Evaluate(ctx context.Context, inputs []string) ([]conftestout.CheckResult, error) {
+	return []conftestout.CheckResult{}, nil
+}
+
+func (e mockEvaluator) Destroy() {
+}
+
+func (b badMockEvaluator) Evaluate(ctx context.Context, inputs []string) ([]conftestout.CheckResult, error) {
+	return nil, errors.New("Evaluator error")
+}
+
+func (e badMockEvaluator) Destroy() {
+}
+
+func mockNewPipelineDefinitionFile(ctx context.Context, fpath string, sources []source.PolicySource) (*definitionfile.DefinitionFile, error) {
+	if fpath == "good" {
+		return &definitionfile.DefinitionFile{
+			Evaluator: mockEvaluator{},
+		}, nil
+
+	}
+	return nil, fmt.Errorf("fpath '%s' does not exist", fpath)
+}
+
+func badMockNewPipelineDefinitionFile(ctx context.Context, fpath string, sources []source.PolicySource) (*definitionfile.DefinitionFile, error) {
+	return &definitionfile.DefinitionFile{
+		Evaluator: badMockEvaluator{},
+	}, nil
+}
+
+func Test_ValidatePipeline(t *testing.T) {
+	tests := []struct {
+		name    string
+		fpath   string
+		err     error
+		output  *output.Output
+		defFunc func(ctx context.Context, fpath string, sources []source.PolicySource) (*definitionfile.DefinitionFile, error)
+	}{
+		{
+			name:    "validation succeeds",
+			fpath:   "good",
+			err:     nil,
+			output:  &output.Output{PolicyCheck: []conftestout.CheckResult{}},
+			defFunc: mockNewPipelineDefinitionFile,
+		},
+		{
+			name:    "validation fails on bad path",
+			fpath:   "bad",
+			err:     fmt.Errorf("fpath '%s' does not exist", "bad"),
+			output:  nil,
+			defFunc: mockNewPipelineDefinitionFile,
+		},
+		{
+			name:    "evaluator fails",
+			fpath:   "good",
+			err:     errors.New("Evaluator error"),
+			output:  nil,
+			defFunc: badMockNewPipelineDefinitionFile,
+		},
+	}
+
+	ctx := utils.WithFS(context.Background(), afero.NewOsFs())
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			def_file = tt.defFunc
+			output, err := ValidateDefinitionFile(ctx, tt.fpath, []source.PolicySource{})
+			assert.Equal(t, tt.err, err)
+			assert.Equal(t, tt.output, output)
+		})
+	}
+}
