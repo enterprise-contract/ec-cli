e784db8d57c94bafa6218fad413db1ecffb24d3b
Zoran Regvart <zoran@regvart.com>
2023-07-31 14:22:08 +0200

    Update license copy

--- hack/update-build-definitions.sh
+++ hack/update-build-definitions.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-# Copyright 2023 Red Hat, Inc.
+# Copyright The Enterprise Contract Contributors
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
--- internal/attestation/attestation.go
+++ internal/attestation/attestation.go
@@ -1,4 +1,4 @@
-// Copyright 2023 Red Hat, Inc.
+// Copyright The Enterprise Contract Contributors
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
