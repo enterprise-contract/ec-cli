enterprise-contract/ec-cli

commit 9015ea5852ff59d515742ce3bd53ef3444499997

Don't require license preamble on TAP pipelines
Split the long line for readability. The last two lines are the new
ones.

Makefile
@@ -144,8 +144,17 @@ ci: test lint-fix acceptance ## Run the usual required CI tasks
 
 ##@ Linters
 
-LICENSE_IGNORE=-ignore 'dist/cli-reference/*.yaml' -ignore 'acceptance/examples/*.yaml' -ignore 'configs/*/*.yaml' -ignore 'node_modules/**' -ignore 'hack/**/charts/**'
+LICENSE_IGNORE=\
+-ignore 'dist/cli-reference/*.yaml' \
+-ignore 'acceptance/examples/*.yaml' \
+-ignore 'configs/*/*.yaml' \
+-ignore 'node_modules/**' \
+-ignore 'hack/**/charts/**' \
+-ignore '.tekton/ec-cli-pull-request.yaml \
+-ignore '.tekton/ec-cli-push.yaml
+
 LINT_TO_GITHUB_ANNOTATIONS='map(map(.)[])[][] as $$d | $$d.posn | split(":") as $$posn | "::warning file=\($$posn[0]),line=\($$posn[1]),col=\($$posn[2])::\($$d.message)"'
+
 .PHONY: lint
 lint: tekton-lint go-mod-lint ## Run linter
 # addlicense doesn't give us a nice explanation so we prefix it with one

commit 712ef527e2e43927cae21e0709736f4be26c7d16

Fix missing quotes LICENSE_IGNORE file list
This bug was introduced in 9015ea5 but not noticed until now.

Makefile
@@ -154,8 +154,8 @@ LICENSE_IGNORE=\
 -ignore 'configs/*/*.yaml' \
 -ignore 'node_modules/**' \
 -ignore 'hack/**/charts/**' \
--ignore '.tekton/ec-cli-pull-request.yaml \
--ignore '.tekton/ec-cli-push.yaml
+-ignore '.tekton/ec-cli-pull-request.yaml' \
+-ignore '.tekton/ec-cli-push.yaml'
 
 LINT_TO_GITHUB_ANNOTATIONS='map(map(.)[])[][] as $$d | $$d.posn | split(":") as $$posn | "::warning file=\($$posn[0]),line=\($$posn[1]),col=\($$posn[2])::\($$d.message)"'
 