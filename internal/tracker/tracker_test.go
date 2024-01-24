0a4973b26ee931029b68da67ac8b0a142c9a822f
Luiz Carvalho <lucarval@redhat.com>
2024-01-24 15:43:22 -0500

    Re-enable tests for tracker package
    These tests were accidentally disabled on Sep 28th. Fortunately, they
    are all still passing.
    
    Signed-off-by: Luiz Carvalho <lucarval@redhat.com>

--- internal/tracker/tracker_test.go
+++ internal/tracker/tracker_test.go
@@ -436,7 +436,7 @@ func TestTrack(t *testing.T) {
 		},
 	}
 
-	for _, tt := range tests[len(tests)-1:] {
+	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
 			ctx := context.WithValue(context.Background(), image.RemoteHead, head)
 
