ee31dc104751971c2eed4b8d4956087b317fcb0c
Luiz Carvalho <lucarval@redhat.com>
2024-02-20 17:00:53 -0500

    wip: ec.oci.image_manifest static object approach
    Signed-off-by: Luiz Carvalho <lucarval@redhat.com>

--- internal/evaluator/rego.go
+++ internal/evaluator/rego.go
@@ -29,6 +29,7 @@ import (
 
 	"github.com/google/go-containerregistry/pkg/authn"
 	"github.com/google/go-containerregistry/pkg/name"
+	v1 "github.com/google/go-containerregistry/pkg/v1"
 	"github.com/google/go-containerregistry/pkg/v1/remote"
 	"github.com/open-policy-agent/opa/ast"
 	"github.com/open-policy-agent/opa/rego"
@@ -75,14 +76,58 @@ func registerOCIBlob() {
 }
 
 func registerOCIImageManifest() {
+	platform := types.NewObject(
+		[]*types.StaticProperty{
+			{Key: "architecture", Value: types.S},
+			{Key: "os", Value: types.S},
+			{Key: "os.version", Value: types.S},
+			{Key: "os.features", Value: types.NewArray([]types.Type{types.S}, nil)},
+			{Key: "variant", Value: types.S},
+			{Key: "features", Value: types.NewArray([]types.Type{types.S}, nil)},
+		},
+		nil,
+	)
+
+	// annotations represents the map[string]string rego type
+	annotations := types.NewObject(nil, types.NewDynamicProperty(types.S, types.S))
+
+	descriptor := types.NewObject(
+		[]*types.StaticProperty{
+			{Key: "mediaType", Value: types.S},
+			{Key: "size", Value: types.N},
+			{Key: "digest", Value: types.S},
+			{Key: "data", Value: types.S},
+			{Key: "urls", Value: types.NewArray(
+				[]types.Type{types.S}, nil,
+			)},
+			{Key: "annotations", Value: annotations},
+			{Key: "platform", Value: platform},
+			{Key: "artifactType", Value: types.S},
+		},
+		nil,
+	)
+
 	decl := rego.Function{
 		Name: ociImageManifestName,
 		Decl: types.NewFunction(
 			types.Args(
 				types.Named("ref", types.S).Description("OCI image reference"),
 			),
-			// TODO: Define the exact attributes the object will have.
-			types.Named("manifest", types.S).Description("the OCI Image Manifest"),
+			types.Named("object", types.NewObject(
+				[]*types.StaticProperty{
+					// Specifying the properties like this ensure the compiler catches typos when
+					// evaluating rego functions.
+					{Key: "schemaVersion", Value: types.N},
+					{Key: "mediaType", Value: types.S},
+					{Key: "config", Value: descriptor},
+					{Key: "layers", Value: types.NewArray(
+						[]types.Type{descriptor}, nil,
+					)},
+					{Key: "annotations", Value: annotations},
+					{Key: "subject", Value: descriptor},
+				},
+				nil,
+			)).Description("the Image Manifest object"),
 		),
 		// As per the documentation, enable memoization to ensure function evaluation is
 		// deterministic. But also mark it as non-deterministic because it does rely on external
@@ -267,14 +312,76 @@ func ociImageManifest(bctx rego.BuiltinContext, a *ast.Term) (*ast.Term, error) 
 		return nil, nil
 	}
 
-	// TODO: Don't really like marshaling and unmarshaling here... But it might be a good compromise?
-	manifestJSON, err := json.Marshal(manifest)
-	if err != nil {
-		log.Errorf("%s json Marshal: %s", ociImageManifestName, err)
-		return nil, nil
+	layers := []*ast.Term{}
+	for _, layer := range manifest.Layers {
+		layers = append(layers, newDescriptorTerm(layer))
 	}
 
-	return ast.StringTerm(string(manifestJSON)), nil
+	manifestTerms := [][2]*ast.Term{
+		ast.Item(ast.StringTerm("schemaVersion"), ast.NumberTerm(json.Number(fmt.Sprintf("%d", manifest.SchemaVersion)))),
+		ast.Item(ast.StringTerm("mediaType"), ast.StringTerm(string(manifest.MediaType))),
+		ast.Item(ast.StringTerm("config"), newDescriptorTerm(manifest.Config)),
+		ast.Item(ast.StringTerm("layers"), ast.ArrayTerm(layers...)),
+		ast.Item(ast.StringTerm("annotations"), newAnnotationsTerm(manifest.Annotations)),
+	}
+
+	if s := manifest.Subject; s != nil {
+		manifestTerms = append(manifestTerms, ast.Item(ast.StringTerm("subject"), newDescriptorTerm(*s)))
+	}
+
+	return ast.ObjectTerm(manifestTerms...), nil
+}
+
+func newPlatformTerm(p v1.Platform) *ast.Term {
+	osFeatures := []*ast.Term{}
+	for _, f := range p.OSFeatures {
+		osFeatures = append(osFeatures, ast.StringTerm(f))
+	}
+
+	features := []*ast.Term{}
+	for _, f := range p.Features {
+		features = append(features, ast.StringTerm(f))
+	}
+
+	return ast.ObjectTerm(
+		ast.Item(ast.StringTerm("architecture"), ast.StringTerm(p.Architecture)),
+		ast.Item(ast.StringTerm("os"), ast.StringTerm(p.OS)),
+		ast.Item(ast.StringTerm("os.version"), ast.StringTerm(p.OSVersion)),
+		ast.Item(ast.StringTerm("os.features"), ast.ArrayTerm(osFeatures...)),
+		ast.Item(ast.StringTerm("variant"), ast.StringTerm(p.Variant)),
+		ast.Item(ast.StringTerm("features"), ast.ArrayTerm(features...)),
+	)
+}
+
+func newDescriptorTerm(d v1.Descriptor) *ast.Term {
+	urls := []*ast.Term{}
+	for _, url := range d.URLs {
+		urls = append(urls, ast.StringTerm(url))
+	}
+
+	dTerms := [][2]*ast.Term{
+		ast.Item(ast.StringTerm("mediaType"), ast.StringTerm(string(d.MediaType))),
+		ast.Item(ast.StringTerm("size"), ast.NumberTerm(json.Number(fmt.Sprintf("%d", d.Size)))),
+		ast.Item(ast.StringTerm("digest"), ast.StringTerm(d.Digest.String())),
+		ast.Item(ast.StringTerm("data"), ast.StringTerm(string(d.Data))),
+		ast.Item(ast.StringTerm("urls"), ast.ArrayTerm(urls...)),
+		ast.Item(ast.StringTerm("annotations"), newAnnotationsTerm(d.Annotations)),
+		ast.Item(ast.StringTerm("artifactType"), ast.StringTerm(d.ArtifactType)),
+	}
+
+	if d.Platform != nil {
+		dTerms = append(dTerms, ast.Item(ast.StringTerm("platform"), newPlatformTerm(*d.Platform)))
+	}
+
+	return ast.ObjectTerm(dTerms...)
+}
+
+func newAnnotationsTerm(annotations map[string]string) *ast.Term {
+	annotationTerms := [][2]*ast.Term{}
+	for key, value := range annotations {
+		annotationTerms = append(annotationTerms, ast.Item(ast.StringTerm(key), ast.StringTerm(value)))
+	}
+	return ast.ObjectTerm(annotationTerms...)
 }
 
 func purlIsValid(bctx rego.BuiltinContext, a *ast.Term) (*ast.Term, error) {
