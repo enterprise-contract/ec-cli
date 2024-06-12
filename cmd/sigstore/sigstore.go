57aa030b317aad95c049de105f0e0a792517eae7
Luiz Carvalho <lucarval@redhat.com>
2024-06-12 13:18:55 -0400

    Add sigstore initialize subcommand
    It is now possible to initialize the TUF root directly with the ec-cli.
    No need to use another binary, e.g. cosign, to perform this operation.
    
    Ref: EC-584
    
    Signed-off-by: Luiz Carvalho <lucarval@redhat.com>

---
+++ cmd/sigstore/sigstore.go
@@ -0,0 +1,39 @@
+// Copyright The Enterprise Contract Contributors
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+//
+// SPDX-License-Identifier: Apache-2.0
+
+package sigstore
+
+import (
+	"github.com/sigstore/cosign/v2/cmd/cosign/cli/initialize"
+	"github.com/spf13/cobra"
+
+	_ "github.com/enterprise-contract/ec-cli/internal/rego"
+)
+
+var SigstoreCmd *cobra.Command
+
+func init() {
+	SigstoreCmd = NewSigstoreCmd()
+	SigstoreCmd.AddCommand(sigstoreInitializeCmd(initialize.DoInitialize))
+}
+
+func NewSigstoreCmd() *cobra.Command {
+	sigstoreCmd := &cobra.Command{
+		Use:   "sigstore",
+		Short: "Perform certain sigstore operations",
+	}
+	return sigstoreCmd
+}
