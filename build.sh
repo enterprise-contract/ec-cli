enterprise-contract/ec-cli

commit b8db99c2d4c8b8df50df00a230ab1df792ebf181

let the Dockerfile determine the build type

build.sh
@@ -40,9 +40,7 @@
 # a PR is merged.
 #
 
-BUILD_LIST="${1:-darwin_amd64 darwin_arm64 linux_amd64 linux_arm64 linux_ppc64le linux_s390x windows_amd64}"
-TARGETOS=$2
-TARGETARCH=$3
+BUILDS="${1}"
 
 EC_GIT_ORIGIN_BRANCH=$( git for-each-ref --points-at HEAD --format='%(refname:short)' refs/remotes/origin/ )
 echo "EC_GIT_ORIGIN_BRANCH=$EC_GIT_ORIGIN_BRANCH"
@@ -54,14 +52,11 @@ EC_VERSION=${EC_GIT_BRANCH#"release-"}
 if [[ "${EC_GIT_BRANCH}" != release-* ]]; then
     EC_VERSION=v0.3
     EC_PATCH_NUM=$(git rev-list --count HEAD)-$(git rev-parse --short HEAD)
-    BUILDS="${TARGETOS}_${TARGETARCH}"
 else
     EC_BASE_TAG="${EC_VERSION}.0"
     echo "EC_BASE_TAG=$EC_BASE_TAG"
     git log -n1 --format="%h %s" "$EC_BASE_TAG"
-
     EC_PATCH_NUM=$( git rev-list --merges --count ${EC_BASE_TAG}..HEAD )
-    BUILDS="${BUILD_LIST}"
 fi
 
 EC_FULL_VERSION="${EC_VERSION}.${EC_PATCH_NUM}"
@@ -72,8 +67,8 @@ echo "EC_FULL_VERSION=$EC_FULL_VERSION"
 echo "BUILDS=$BUILDS"
 
 for os_arch in ${BUILDS}; do
-    export GOOS="${os_arch%_*}"
-    export GOARCH="${os_arch#*_}"
+    GOOS="${os_arch%_*}"
+    GOARCH="${os_arch#*_}"
     [[ "$GOOS" == "windows" ]] && DOT_EXE=".exe" || DOT_EXE=""
     BINFILE="ec_${GOOS}_${GOARCH}${DOT_EXE}"
     echo "Building ${BINFILE} for ${EC_FULL_VERSION}"

Dockerfile
@@ -18,7 +18,6 @@
 
 FROM docker.io/library/golang:1.21 AS build
 
-ARG BUILD_LIST="darwin_amd64 darwin_arm64 linux_amd64 linux_arm64 linux_ppc64le linux_s390x windows_amd64"
 ARG TARGETOS
 ARG TARGETARCH
 
@@ -31,7 +30,7 @@ RUN go mod download
 # Now copy everything including .git
 COPY . .
 
-RUN ["/usr/bin/bash", "-c", "/build/build.sh \"${BUILD_LIST}\" \"${TARGETOS}\" \"${TARGETARCH}\""]
+RUN /build/build.sh "${TARGETOS}_${TARGETARCH}"
 
 # Extract this so we can download the matching cosign version below
 RUN go list --mod=readonly -f '{{.Version}}' -m github.com/sigstore/cosign/v2 | tee cosign_version.txt
@@ -64,8 +63,6 @@ COPY --from=download /download/cosign /usr/bin/cosign
 RUN cosign version
 
 RUN microdnf -y install git-core jq && microdnf clean all
-
-COPY --from=build /build/dist/ /usr/bin/
 
 # Copy the one ec binary that can run in this container
 COPY --from=build "/build/dist/ec_${TARGETOS}_${TARGETARCH}" /usr/bin/ec

Dockerfile.dist
@@ -41,7 +41,7 @@ RUN go mod download
 # Now copy everything including .git
 COPY . .
 
-RUN ["/usr/bin/bash", "-c", "/build/build.sh \"${BUILD_LIST}\" \"${TARGETOS}\" \"${TARGETARCH}\""]
+RUN /build/build.sh "${BUILD_LIST}"
 
 # Extract this so we can download the matching cosign version below
 RUN go list --mod=readonly -f '{{.Version}}' -m github.com/sigstore/cosign/v2 | tee cosign_version.txt

commit d62ea87fc5bbf9ff8367faf6429ee9a1045fc5f2

Fix Konflux build platform arch
The GOOS and GOARCH env vars need to be set explicitly, otherwise we
just build the linux x86 binary over and over. Looks like this
regression was introduced in commit b8db99c2.

I think restoring the export on those two vars would also have
worked.

Ref: https://issues.redhat.com/browse/SECURESIGN-1054

build.sh
@@ -35,7 +35,7 @@ for os_arch in ${BUILDS}; do
     [[ "$GOOS" == "windows" ]] && DOT_EXE=".exe" || DOT_EXE=""
     BINFILE="ec_${GOOS}_${GOARCH}${DOT_EXE}"
     echo "Building ${BINFILE} for ${EC_FULL_VERSION}"
-    go build \
+    GOOS="${GOOS}" GOARCH="${GOARCH}" go build \
         -trimpath \
         --mod=readonly \
         -ldflags="-s -w -X github.com/enterprise-contract/ec-cli/internal/version.Version=${EC_FULL_VERSION}" \