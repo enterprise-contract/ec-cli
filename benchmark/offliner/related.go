10c5e06c98bacc8af6414fd0b0d0bf012f95ee3d
Zoran Regvart <zoran@regvart.com>
2024-12-03 16:15:13 +0100

    Simple macro benchmark
    Implementation of a simple macro benchmark executing `ec validate image`
    with the fixed state and no external dependencies.
    
    For this the OCI distribution registry is run in a container with the
    data from the `benchmark/simple/data.tar.gz` that contains the copy of
    all images/blobs that the command and the policy rules currently access
    and the git repository.
    
    The benchmark outputs in the golang standard benchmark format that can
    be utilized with tools in the golang benchmarking ecosystem.
    
    Due to the global `downloadCache` variable in
    `internal/policy/source/source.go` the benchmark cannot be run in
    parallel. So only invoking the benchmark once is currently possible.
    
    Reference: https://issues.redhat.com/browse/EC-968

---
+++ benchmark/offliner/related.go
@@ -0,0 +1,91 @@
+// Copyright The Enterprise Contract Contributors
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+//
+// SPDX-License-Identifier: Apache-2.0
+
+package main
+
+import (
+	"log"
+	"slices"
+	"strings"
+
+	"github.com/google/go-containerregistry/pkg/name"
+	"github.com/google/go-containerregistry/pkg/v1/remote"
+)
+
+type relatedFn func(ref name.Reference) ([]name.Reference, error)
+
+func related(ref name.Reference) ([]name.Reference, error) {
+	references := []name.Reference{ref}
+
+	for _, r := range []name.Reference{
+		signature(ref),
+		attestation(ref),
+		sbom(ref),
+	} {
+		if _, err := remote.Image(r); err == nil {
+			references = append(references, r)
+		} else {
+			log.Printf("Can't dereference %q: %v", r, err)
+		}
+	}
+
+	related := []relatedFn{
+		referrers,
+		baseImages,
+		scanReports,
+		subjects,
+	}
+
+	for _, rel := range related {
+		refs, err := rel(ref)
+		if err != nil {
+			log.Printf("Can't fetch related image for %q: %v", ref, err)
+			continue
+		}
+		references = append(references, refs...)
+	}
+
+	cmp := func(a, b name.Reference) int {
+		return strings.Compare(a.String(), b.String())
+	}
+
+	for _, ref := range references {
+		if _, ok := ref.(name.Digest); !ok {
+			img, err := remote.Image(ref)
+			if err != nil {
+				return nil, err
+			}
+
+			digest, err := img.Digest()
+			if err != nil {
+				return nil, err
+			}
+
+			ref = ref.Context().Digest(digest.String())
+		}
+
+		ref := signature(ref)
+		if _, err := remote.Image(ref); err == nil {
+			references = append(references, ref)
+		} else {
+			log.Printf("Can't dereference %q: %v", ref, err)
+		}
+	}
+
+	slices.SortFunc(references, cmp)
+
+	return slices.Compact(references), nil
+}
---
+++ benchmark/offliner/scans.go
@@ -0,0 +1,117 @@
+// Copyright The Enterprise Contract Contributors
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+//
+// SPDX-License-Identifier: Apache-2.0
+
+package main
+
+import (
+	"encoding/base64"
+	"encoding/json"
+
+	"github.com/google/go-containerregistry/pkg/name"
+	"github.com/google/go-containerregistry/pkg/v1/remote"
+	"github.com/in-toto/in-toto-golang/in_toto"
+	"github.com/secure-systems-lab/go-securesystemslib/dsse"
+)
+
+func scanReports(ref name.Reference) ([]name.Reference, error) {
+	att := attestation(ref)
+
+	img, err := remote.Image(att)
+	if err != nil {
+		return nil, err
+	}
+
+	layers, err := img.Layers()
+	if err != nil {
+		return nil, err
+	}
+
+	refs := make([]name.Reference, 0, 5)
+	for _, layer := range layers {
+		if mt, err := layer.MediaType(); err != nil || mt != "application/vnd.dsse.envelope.v1+json" {
+			continue
+		}
+
+		data, err := layer.Uncompressed()
+		if err != nil {
+			return nil, err
+		}
+		defer data.Close()
+
+		var envelope dsse.Envelope
+		decoder := json.NewDecoder(data)
+		if err := decoder.Decode(&envelope); err != nil {
+			return nil, err
+		}
+
+		if envelope.PayloadType != "application/vnd.in-toto+json" {
+			continue
+		}
+
+		raw, err := base64.StdEncoding.DecodeString(envelope.Payload)
+		if err != nil {
+			return nil, err
+		}
+
+		var statement in_toto.ProvenanceStatementSLSA02
+		if err := json.Unmarshal(raw, &statement); err != nil {
+			return nil, err
+		}
+
+		if statement.PredicateType != "https://slsa.dev/provenance/v0.2" {
+			continue
+		}
+
+		subject, err := name.NewRepository(statement.Subject[0].Name)
+		if err != nil {
+			return nil, err
+		}
+
+		buildConfig := statement.Predicate.BuildConfig
+
+		tasks := buildConfig.(map[string]any)["tasks"].([]any)
+
+		for _, task := range tasks {
+			task := task.(map[string]any)
+			// TODO match by reference instead of name
+			if task["name"] != "clair-scan" {
+				continue
+			}
+
+			results := task["results"].([]any)
+
+			for _, result := range results {
+				result := result.(map[string]any)
+				if result["name"] != "REPORTS" {
+					continue
+				}
+
+				report := result["value"]
+
+				var reports map[string]string
+				if err := json.Unmarshal([]byte(report.(string)), &reports); err != nil {
+					return nil, err
+				}
+
+				for _, digest := range reports {
+					refs = append(refs, subject.Digest(digest))
+				}
+			}
+		}
+	}
+
+	return refs, nil
+}
